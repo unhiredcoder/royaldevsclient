1f30281cfab0cf8861506a4734a005dfec44d4d1
unhiredcoder <proboys777333@gmail.com>
2023-12-16 21:53:25 +0530

    SOME CHANGES IN STYLES

--- components/BlogCardWithImage.tsx
+++ components/BlogCardWithImage.tsx
@@ -2,14 +2,22 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { IArticle } from '../types';
+
 interface IPropType {
     article: IArticle;
 }
+
 const BlogCardWithImage = ({ article }: IPropType) => {
     return (
-        <div className="rounded-md flex items-center h-64 bg-gradient-to-br from-green-300 to-yellow-400">
+        <div
+            className="rounded-md flex items-center h-64"
+            style={{
+                background:
+                    'linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%)',
+            }}
+        >
             <Link href={`/article/${article.attributes.slug}`}>
-                <span className="text-2xl w-2/3 text-[#4169E1] p-6 font-bold after:content-[''] after:bg-black after:block after:w-16 after:h-1 after:rounded-md after:mt-2 cursor-pointer">
+                <span className="text-2xl w-2/3 text-[#FFF] p-6 font-bold after:content-[''] after:bg-[#FFF] after:block after:w-16 after:h-1 after:rounded-md after:mt-2 cursor-pointer">
                     {article.attributes.title}
                 </span>
             </Link>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
                     <img src="/logo.png"  className='mob-foot' style={{height:'20%',width:'15%'}} />
                 </Link>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-                    © 2022 Royal Developers —
+                    © 2024 Royal Developers —
                     <a
                         href="https://twitter.com"
                         className="text-gray-600 ml-1"
